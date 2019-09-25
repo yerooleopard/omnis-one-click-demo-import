e8ff0e6604192586fb5fb4a783f59b7c4b9c4c42
Adrian Lampart <yeroo.s3@gmail.com>
2019-09-25 16:27:29 +0200

    INIT

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt){
+
+	// Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		compress: {
+		  	main: {
+			    options: {
+			      	archive: '_zip/<%= pkg.name %>.zip'
+			    },
+				files: [{
+	                expand: true,
+	                src: ['**/**', 
+	                	'!node_modules/**',
+	                	'!.sass-cache',
+						'!.vscode',
+						'!.gitignore',
+						'!.git',
+	                	'!<%= pkg.name %>.zip',
+	                	'!_zip/**',
+	                	'!_toRemove/**'
+	                ],
+	                dest: '<%= pkg.name %>/'
+                }]
+		  	}
+		},		
+
+	});
+
+	// Load plugins
+	grunt.loadNpmTasks('grunt-contrib-compress');
+
+	// Register Task
+	grunt.registerTask('default', ['compress']);
+	grunt.registerTask('zip', ['compress']);
+
+
+};	
